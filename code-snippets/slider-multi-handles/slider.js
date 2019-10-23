b80a06336fe5605be0ee7fb3f33c8b5f641acd67
Barry Hood <barry.hood@connect-group.com>
2019-10-23 15:59:49 +0100

    Destroy/recreate methods

--- code-snippets/slider-multi-handles/slider.js
+++ code-snippets/slider-multi-handles/slider.js
@@ -1,113 +1,10 @@
-
-// class DxImageSlider {
-//   constructor(element, start) {
-
-//     this.element = element;
-//     this.customConnect = {
-//       before: null,
-//       after: null
-//     };
-//     this.customConnectSpacing = 27;
-
-//     this.createCustomConnects();
-
-//     this.slider = noUiSlider.create(this.element, {
-//         start,
-//         // connect: [1,1],
-//         range: {
-//             'min': 0,
-//             'max': 10
-//         },
-//         customSpacing: 27,
-//     });
-
-//     console.log('slider:', this.slider);
-
-//     this.addCustomConnectEvents();
-//   }
-
-//   createCustomConnects() {
-//     const createConnect = className => {
-//       const element = document.createElement('div');
-//       element.classList.add(className);
-//       return element;
-//     };
-
-//     this.customConnect.before = createConnect('DxImageSlider__connect-before');
-//     this.customConnect.after = createConnect('DxImageSlider__connect-after');
-
-//     this.element.appendChild(this.customConnect.before);
-//     this.element.appendChild(this.customConnect.after);
-//   }
-
-//   addCustomConnectEvents() {
-//     const customBefore = this.customConnect.before;
-//     const customAfter = this.customConnect.after;
-//     const getWidthCalc = value => `calc(${value}% - ${this.customConnectSpacing}px)`;
-
-//     this.slider.on('update.custom-connects', () => {
-//       const sliderCurrent = parseFloat(this.element.noUiSlider.get());
-//       customBefore.style.width = getWidthCalc(this.getValueAsPercentage(sliderCurrent));
-//       customAfter.style.width = getWidthCalc(100 - this.getValueAsPercentage(sliderCurrent));
-//     });
-//   }
-
-//   getValueAsPercentage(value) {
-//     const { min, max } = this.element.noUiSlider.options.range;
-//     return ((value - min) * 100) / (max - min);
-//   }
-// }
-
-
-
-/*
-  Usage: pass an element as the first parameter, the second parameter is an options object - this is based on
-  the noUiSlider options: https://refreshless.com/nouislider/slider-options/
-
-  If `start` is a single value, a one handle slider will be created. If `start` is an array of two values, a
-  dual handle slider will be created. These are styled differently as per the Spark44 slider mini-briefs.
-
-  NOTE: it is recommended not to change the connect values. This utility method hides the standard connects and
-  creates its own custom connects in order to allow for the clipped spacing between the connects and the handle.
-  Additionally, more than two handles are not currently supported as this was not a use case in the mini briefs.
-
-  To hook into the slider events, use the noUiSlider events: https://refreshless.com/nouislider/events-callbacks/
-  noUiSlider provided custom binding with namespacing and has two methods:
-
-  To bind: `.on(event, callback)`
-  To unbind: `.off(event)`
-
-  Basic example of creating a slider and binding to the update event with custom namespacing:
-
-  const slider = document.querySelector('.yourSlider');
-  const options = {
-    start: 50,
-    range: { min: 0, max: 100 }
-  };
-  new DxRangeSlider(slider, options);
-
-  // bind a custom namespaced event listener to the slider update event:
-  slider.noUiSlider.on('update.myCustomNamespacing',  (values, handle, unencoded, tap, positions) => {
-    // do something
-  });
-
-  // remove all custom namespaced event listeners
-  slider.noUiSlider.off('.myCustomNamespacing');
-
-  // values: Current slider values as an array, with formatting applied;
-  // handle: Index number of the handle which caused the event to fire;
-  // unencoded: Slider values as an array without formatting;
-  // tap: (boolean) Is false if the user dragged the handle, true if the user tapped on the slider range;
-  // positions: Left offset values of the handles as an array;
-*/
-
 class DxRangeSlider {
   constructor(element, options) {
     this.element = element;
     this.customConnectClass = 'DxRangeSlider__custom-connect';
     this.customConnectIndex = 'data-dxrangeslider-index';
     this.customConnectNamespace = 'DxRangeSlider-custom-connects';
-
+    this.initAttribute = 'data-dxrangeslider-init';
     this.smallHandleClass = 'DxRangeSlider--small-handles';
     this.smallHandles = options.start.length > 1;
 
@@ -123,6 +20,10 @@ class DxRangeSlider {
     this.createSlider();
   }
 
+  updateOptions(options) {
+    this.options = Object.assign(this.options, options);
+  }
+
   createStartArray() {
     const start = this.options.start;
     return Array.isArray(start) ? start : [start];
@@ -136,6 +37,7 @@ class DxRangeSlider {
   }
 
   addCustomClasses() {
+    this.element.classList.add(`data-dxrangeslider-direction="${this.options.direction}"`);
     if (this.smallHandles) {
       this.element.classList.add(this.smallHandleClass);
     }
@@ -143,6 +45,7 @@ class DxRangeSlider {
 
   initSlider() {
     this.slider = noUiSlider.create(this.element, this.options);
+    this.setInitStatus('true');
   }
 
   destroySlider() {
@@ -154,6 +57,16 @@ class DxRangeSlider {
 
     // call noUiSlider destroy method
     this.slider.destroy();
+    this.setInitStatus(null);
+  }
+
+  getInitStatus() {
+    console.log('this.element.getAttribute(this.initAttribute):', !!this.element.getAttribute(this.initAttribute));
+    return this.element.getAttribute(this.initAttribute);
+  }
+
+  setInitStatus(status) {
+    this.element.setAttribute(this.initAttribute, status ? true : '');
   }
 
   createCustomConnects() {
@@ -169,7 +82,6 @@ class DxRangeSlider {
 
 
   addCustomConnectEvents() {
-
     const alignment = this.options.direction === 'ltr' ? 'left' : 'right';
     const connectors = [...this.element.querySelectorAll(`[${this.customConnectIndex}]`)];
     const firstConnector = connectors[0];
@@ -198,6 +110,7 @@ class DxRangeSlider {
 }
 
 
+// EXAMPLES
 const sliders = document.querySelectorAll('.DxRangeSlider');
 
 const opts = {
@@ -207,12 +120,15 @@ const opts = {
     max: 10
   }
 };
+
+// single handle
 const slider1 = new DxRangeSlider(sliders[0], opts);
 
+// dual handle
 opts.start = [3, 7];
 let slider2 = new DxRangeSlider(sliders[1], opts);
 
-
+// EXAMPLE ABILITY TO DESTROY/RECREATE A SLIDER
 const destroy = document.querySelector('.destroy');
 const recreate = document.querySelector('.recreate');
 
@@ -223,5 +139,15 @@ destroy.addEventListener('click', (event) => {
 
 recreate.addEventListener('click', (event) => {
   event.preventDefault();
-  slider2 = new DxRangeSlider(sliders[1], opts);
+  // slider2 = new DxRangeSlider(sliders[1], opts);
+  slider2.updateOptions({
+    start: [1, 10]
+  });
+  slider2.createSlider();
+});
+
+const getInit = document.querySelector('.init');
+getInit.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log(slider2.getInitStatus());
 });
